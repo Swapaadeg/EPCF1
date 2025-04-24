32fc2995d7b335509a013552bc7b8912b15917ef
Marie Rivier <marie.rivier23@gmail.com>
2025-04-24 10:15:31 +0200

    footer

---
+++ assets/scripts/script.js
@@ -0,0 +1,7 @@
+// input.addEventListener("blur", () => {
+//     if (input.value.trim() === "") {
+//       input.nextElementSibling.textContent = "Ce champ est requis.";
+//     } else {
+//       input.nextElementSibling.textContent = "";
+//     }
+//   });
